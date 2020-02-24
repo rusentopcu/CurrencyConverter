8bfc2a1782818184fdeba97789239a41ea27f5c2
Rusen Topcu <rusentopcu@msn.com>
2020-02-25 02:14:55 +0300

    User Interface prepare

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: - App için faydalanılan url main adress: (https://fixer.io)
+        
     }
 
-
+    @IBAction func getRates(_ sender: Any) {
+        
+        
+        
+    }
+    
 }
 
